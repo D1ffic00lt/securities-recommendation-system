7602325c95aba513afec0484dce0d4f96335e993
D1ffic00lt <dm.filinov@gmail.com>
2024-11-06 19:19:44 +0300

    input data optionality added

--- preprocessing/rating.py
+++ preprocessing/rating.py
@@ -3,7 +3,9 @@ import numpy as np
 
 from datetime import datetime
 from typing import Any, Iterable
-from pandas import Series
+from matplotlib.cbook import boxplot_stats
+
+from preprocessing.transformers import make_empty_values_filler_pipeline
 
 
 class SecurityPriceRating(object):
@@ -88,52 +90,147 @@ class SecurityPriceRating(object):
 
 
 class NormalizedData(dict):
-    def __init__(self, data: pd.DataFrame, keys: Iterable[str], *args, **kwargs):
+    def __init__(
+        self,
+        data: pd.DataFrame,
+        keys: Iterable[str],
+        *args,
+        process_outliers: bool = True,
+        **kwargs,
+    ):
         super().__init__(*args, **kwargs)
+        if data.shape[0] == 0:
+            return
+
         for key in keys:
             match key:
                 case "ipo_date":
-                    self[key] = self.normalize(
-                        (datetime.now() - self.to_datetime(data[key])).apply(
-                            lambda x: x.days
-                        )
+                    dict.__setitem__(
+                        self,
+                        key,
+                        self.normalize(
+                            (datetime.now() - self.to_datetime(data[key])).apply(
+                                lambda x: x.days
+                            )
+                        ),
                     )
                 case "maturity_date":
-                    self[key] = self.normalize(
-                        (self.to_datetime(data[key]) - datetime.now()).apply(lambda x: x.days)
+                    dict.__setitem__(
+                        self,
+                        key,
+                        self.normalize(
+                            (self.to_datetime(data[key]) - datetime.now()).apply(
+                                lambda x: x.days
+                            )
+                        ),
                     )
                 case _:
-                    self[key] = self.normalize(data[key])
+                    if not process_outliers:
+                        dict.__setitem__(self, key, self.normalize(data[key]))
+                    outliers_indexes = self._get_outliers_indexes(data[key])
+                    column = data[key].copy()
+                    column[outliers_indexes] = 10
+                    column[~outliers_indexes] = self.normalize(data[key])
+                    dict.__setitem__(self, key, column)
+
+    def __setitem__(self, key, value):
+        raise KeyError("You can't change the attributes.")
 
     @staticmethod
-    def to_datetime(value: pd.Series) -> Series:
+    def to_datetime(value: pd.Series) -> pd.Series:
         return pd.to_datetime(value, format="%d/%m/%Y")
 
     @staticmethod
     def normalize(series: pd.Series) -> pd.Series:
         return 10 * (series - series.min()) / (series.max() - series.min())
 
+    @staticmethod
+    def _get_outliers_indexes(data: pd.Series) -> pd.Series:
+        outliers = boxplot_stats(data).pop(0)["fliers"]
+        return data.isin(outliers)
+
 
 class SecurityRating(object):
     def __init__(
-        self, shares: pd.DataFrame, bonds: pd.DataFrame, etfs: pd.DataFrame
+        self,
+        shares: pd.DataFrame = None,
+        bonds: pd.DataFrame = None,
+        etfs: pd.DataFrame = None,
+        *,
+        remove_nan_values: bool = True,
+        process_outliers: bool = True,
+        num_nan_value: Any = 0,
+        cat_nan_value: str = "unknown",
     ) -> None:
-        shares = shares.copy()
-        bonds = bonds.copy()
-        etfs = etfs.copy()
+        if all([shares is None, bonds is None, etfs is None]):
+            raise ValueError("At least one DataFrame must be non-empty.")
 
-        if isinstance(shares["ipo_date"].dtype.type, np.object_):
+        shares = shares.copy() if shares is not None else pd.DataFrame()
+        bonds = bonds.copy() if bonds is not None else pd.DataFrame()
+        etfs = etfs.copy() if etfs is not None else pd.DataFrame()
+
+        shares_cat_columns = shares.dtypes[shares.dtypes == "object"].index.tolist()
+        shares_num_columns = shares.dtypes[shares.dtypes != "object"].index.tolist()
+
+        etfs_cat_columns = etfs.dtypes[etfs.dtypes == "object"].index.tolist()
+        etfs_num_columns = etfs.dtypes[etfs.dtypes != "object"].index.tolist()
+
+        bonds_cat_columns = bonds.dtypes[bonds.dtypes == "object"].index.tolist()
+        bonds_num_columns = bonds.dtypes[bonds.dtypes != "object"].index.tolist()
+
+        self._shares_pipeline = make_empty_values_filler_pipeline(
+            num_columns=shares_num_columns,
+            cat_columns=shares_cat_columns,
+            num_nan_value=num_nan_value,
+            cat_nan_value=cat_nan_value,
+        )
+        self._bonds_pipeline = make_empty_values_filler_pipeline(
+            num_columns=bonds_num_columns,
+            cat_columns=bonds_cat_columns,
+            num_nan_value=num_nan_value,
+            cat_nan_value=cat_nan_value,
+        )
+        self._etfs_pipeline = make_empty_values_filler_pipeline(
+            num_columns=etfs_num_columns,
+            cat_columns=etfs_cat_columns,
+            num_nan_value=num_nan_value,
+            cat_nan_value=cat_nan_value,
+        )
+
+        if remove_nan_values:
+            shares = pd.DataFrame(
+                self._shares_pipeline.fit_transform(shares),
+                columns=(shares_num_columns + shares_cat_columns),
+            )
+            bonds = pd.DataFrame(
+                self._bonds_pipeline.fit_transform(bonds),
+                columns=(bonds_num_columns + bonds_cat_columns),
+            )
+            etfs = pd.DataFrame(
+                self._etfs_pipeline.fit_transform(etfs),
+                columns=(etfs_num_columns + etfs_cat_columns),
+            )
+
+        if shares.shape[0] != 0 and shares["ipo_date"].dtype.kind == "O":
             shares["ipo_date"] = pd.to_datetime(shares["ipo_date"], format="%d/%m/%Y")
-        if isinstance(bonds["maturity_date"].dtype.type, np.object_):
+        if bonds.shape[0] != 0 and bonds["maturity_date"].dtype.kind == "O":
             bonds["maturity_date"] = pd.to_datetime(
                 bonds["maturity_date"], format="%d/%m/%Y"
             )
-
-        self._normalized_shares = NormalizedData(shares, ("issue_size", "ipo_date"))
-        self._normalized_etfs = NormalizedData(etfs, ("fixed_commission", "num_shares"))
+        self._normalized_shares = NormalizedData(
+            data=shares,
+            keys=("issue_size", "ipo_date"),
+            process_outliers=process_outliers,
+        )
+        self._normalized_etfs = NormalizedData(
+            data=etfs,
+            keys=("fixed_commission", "num_shares"),
+            process_outliers=process_outliers,
+        )
         self._normalized_bonds = NormalizedData(
-            bonds,
-            ("coupon_quantity_per_year", "maturity_date", "issue_size"),
+            data=bonds,
+            keys=("coupon_quantity_per_year", "maturity_date", "issue_size"),
+            process_outliers=process_outliers,
         )
 
     @staticmethod
@@ -183,6 +280,9 @@ class SecurityRating(object):
                 return 0
 
     def calculate_shares_rating(self, row: pd.Series) -> float:
+        if len(self._normalized_shares.keys()) == 0:
+            raise ValueError("No shares were calculated.")
+
         liquidity = 10 if row["liquidity_flag"] else 0
         country_risk = self._country_risk_score(row["country_of_risk"])
         issue_size = self._normalized_shares["issue_size"][row.name]
@@ -191,6 +291,9 @@ class SecurityRating(object):
         return 0.3 * liquidity + 0.2 * country_risk + 0.3 * issue_size + 0.2 * ipo_days
 
     def calculate_etfs_rating(self, row: pd.Series) -> float:
+        if len(self._normalized_etfs.keys()) == 0:
+            raise ValueError("No etfs were calculated.")
+
         rebalancing_freq = self._rebalancing_freq_score(row["rebalancing_freq"])
         fixed_commission = 10 - self._normalized_etfs["fixed_commission"][row.name]
         focus_type = self._focus_type_score(row["focus_type"])
@@ -204,6 +307,9 @@ class SecurityRating(object):
         )
 
     def calculate_bonds_rating(self, row: pd.Series) -> float:
+        if len(self._normalized_bonds.keys()) == 0:
+            raise ValueError("No bonds were calculated.")
+
         coupon_quantity = self._normalized_bonds["coupon_quantity_per_year"][row.name]
         maturity_date = 10 - self._normalized_bonds["maturity_date"][row.name]
         country_risk = self._country_risk_score(row["country_of_risk"])
