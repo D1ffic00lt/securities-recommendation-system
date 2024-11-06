41221d40e1cdbf8c6d3e6ed5b7b571977b40067b
D1ffic00lt <dm.filinov@gmail.com>
2024-11-06 17:31:41 +0300

    calculation of rating by indicators moved to a separate class

--- preprocessing/rating.py
+++ preprocessing/rating.py
@@ -1,10 +1,12 @@
 import pandas as pd
 import numpy as np
 
-from typing import Any
+from datetime import datetime
+from typing import Any, Iterable
+from pandas import Series
 
 
-class SecurityRating(object):
+class SecurityPriceRating(object):
     def __init__(self, price_data: pd.DataFrame):
         """
         Initialize with price data.
@@ -84,3 +86,132 @@ class SecurityRating(object):
             }
         )
 
+
+class NormalizedData(dict):
+    def __init__(self, data: pd.DataFrame, keys: Iterable[str], *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for key in keys:
+            match key:
+                case "ipo_date":
+                    self[key] = self.normalize(
+                        (datetime.now() - self.to_datetime(data[key])).apply(
+                            lambda x: x.days
+                        )
+                    )
+                case "maturity_date":
+                    self[key] = self.normalize(
+                        (self.to_datetime(data[key]) - datetime.now()).apply(lambda x: x.days)
+                    )
+                case _:
+                    self[key] = self.normalize(data[key])
+
+    @staticmethod
+    def to_datetime(value: pd.Series) -> Series:
+        return pd.to_datetime(value, format="%d/%m/%Y")
+
+    @staticmethod
+    def normalize(series: pd.Series) -> pd.Series:
+        return 10 * (series - series.min()) / (series.max() - series.min())
+
+
+class SecurityRating(object):
+    def __init__(
+        self, shares: pd.DataFrame, bonds: pd.DataFrame, etfs: pd.DataFrame
+    ) -> None:
+        shares = shares.copy()
+        bonds = bonds.copy()
+        etfs = etfs.copy()
+
+        if isinstance(shares["ipo_date"].dtype.type, np.object_):
+            shares["ipo_date"] = pd.to_datetime(shares["ipo_date"], format="%d/%m/%Y")
+        if isinstance(bonds["maturity_date"].dtype.type, np.object_):
+            bonds["maturity_date"] = pd.to_datetime(
+                bonds["maturity_date"], format="%d/%m/%Y"
+            )
+
+        self._normalized_shares = NormalizedData(shares, ("issue_size", "ipo_date"))
+        self._normalized_etfs = NormalizedData(etfs, ("fixed_commission", "num_shares"))
+        self._normalized_bonds = NormalizedData(
+            bonds,
+            ("coupon_quantity_per_year", "maturity_date", "issue_size"),
+        )
+
+    @staticmethod
+    def _country_risk_score(country: str) -> int:
+        match country:
+            case "RU":
+                return 10
+            case "US":
+                return 8
+            case x if x in ["DE", "FR", "EU"]:
+                return 6
+            case "CN":
+                return 4
+            case "KZ":
+                return 5
+            case _:
+                return 2
+
+    @staticmethod
+    def _rebalancing_freq_score(freq: str) -> int:
+        match freq:
+            case "daily":
+                return 10
+            case "quarterly":
+                return 8
+            case "semi_annual":
+                return 5
+            case "annual":
+                return 3
+            case _:
+                return 0
+
+    @staticmethod
+    def _focus_type_score(focus_type: str) -> int:
+        match focus_type:
+            case "equity":
+                return 10
+            case "fixed_income":
+                return 8
+            case "mixed_allocation":
+                return 6
+            case "commodity":
+                return 4
+            case "alternative_investment":
+                return 2
+            case _:
+                return 0
+
+    def calculate_shares_rating(self, row: pd.Series) -> float:
+        liquidity = 10 if row["liquidity_flag"] else 0
+        country_risk = self._country_risk_score(row["country_of_risk"])
+        issue_size = self._normalized_shares["issue_size"][row.name]
+        ipo_days = 10 - self._normalized_shares["ipo_date"][row.name]
+
+        return 0.3 * liquidity + 0.2 * country_risk + 0.3 * issue_size + 0.2 * ipo_days
+
+    def calculate_etfs_rating(self, row: pd.Series) -> float:
+        rebalancing_freq = self._rebalancing_freq_score(row["rebalancing_freq"])
+        fixed_commission = 10 - self._normalized_etfs["fixed_commission"][row.name]
+        focus_type = self._focus_type_score(row["focus_type"])
+        num_shares = self._normalized_etfs["num_shares"][row.name]
+
+        return (
+            0.25 * rebalancing_freq
+            + 0.25 * fixed_commission
+            + 0.3 * focus_type
+            + 0.2 * num_shares
+        )
+
+    def calculate_bonds_rating(self, row: pd.Series) -> float:
+        coupon_quantity = self._normalized_bonds["coupon_quantity_per_year"][row.name]
+        maturity_date = 10 - self._normalized_bonds["maturity_date"][row.name]
+        country_risk = self._country_risk_score(row["country_of_risk"])
+        issue_size = self._normalized_bonds["issue_size"][row.name]
+
+        return (
+            0.25 * coupon_quantity
+            + 0.2 * maturity_date
+            + 0.25 * country_risk
+            + 0.2 * issue_size
+        )
