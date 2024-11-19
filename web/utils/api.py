23e5c4e6be0b377880ec517d126ecaa5da63d00d
D1ffic00lt <dm.filinov@gmail.com>
2024-11-20 02:35:48 +0300

    the server address has been changed to api

--- web/utils/api.py
+++ web/utils/api.py
@@ -2,7 +2,7 @@ import requests
 
 
 def get_portfolio(user_id, capacity):
-    url = f"http://localhost:8002/get-portfolio/{user_id}/{capacity}"
+    url = f"http://api:8002/get-portfolio/{user_id}/{capacity}"
     try:
         response = requests.post(url)
         if response.status_code == 200:
@@ -15,7 +15,7 @@ def get_portfolio(user_id, capacity):
 
 
 def get_price(figis: list[str], how: str = "actual", by: str = "candle_price"):
-    url = f"http://localhost:8002/get-portfolio-price/{how}/{by}"
+    url = f"http://api:8002/get-portfolio-price/{how}/{by}"
     try:
         response = requests.post(url, json=figis)
         if response.status_code == 200:
--- web/utils/portfolio_analysis.py
+++ web/utils/portfolio_analysis.py
@@ -3,14 +3,11 @@ from .api import get_price
 def calculate_summary_statistics(portfolio_df):
     total_value = get_price(portfolio_df.figi.tolist(), how="old")
 
-    sector_distribution = portfolio_df.groupby("sector").count().figi
+    # sector_distribution = portfolio_df.groupby("sector").count().figi
 
-    actual_value = get_price(portfolio_df.figi.tolist())
+    # actual_value = get_price(portfolio_df.figi.tolist())
     summary = {
-        "Общая стоимость портфеля (в рублях)": total_value,
-        "Стоимость через 30 дней": actual_value,
-        "Разница": actual_value - total_value,
-        "Распределение по секторам": sector_distribution.to_dict(),
+        "Общая стоимость портфеля (в рублях)": total_value
     }
 
     return summary
