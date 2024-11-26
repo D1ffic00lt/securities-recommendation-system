c453f9b7b855362b8d427d26516705df912e84b2
D1ffic00lt <dm.filinov@gmail.com>
2024-11-26 13:48:11 +0300

    changes in the web client

--- web/utils/portfolio_analysis.py
+++ web/utils/portfolio_analysis.py
@@ -2,12 +2,8 @@ from .api import get_price
 
 def calculate_summary_statistics(portfolio_df):
     total_value = get_price(portfolio_df.figi.tolist(), how="old")
-
-    # sector_distribution = portfolio_df.groupby("sector").count().figi
-
-    # actual_value = get_price(portfolio_df.figi.tolist())
     summary = {
-        "Общая стоимость портфеля (в рублях)": total_value
+        "Total portfolio value (in roubles)": total_value,
     }
 
     return summary
